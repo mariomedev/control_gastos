f0096eff3b48f04454d315854032efa3acc96aed
Mario Melo <130798353+mariomedev@users.noreply.github.com>
2025-04-22 14:58:11 -0500

    FT: Implement PageView for HomeScreen navigation

--- lib/features/auth/presentation/screens/home/home_screen.dart
+++ lib/features/auth/presentation/screens/home/home_screen.dart
@@ -18,14 +18,33 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
+  PageController pageViewController = PageController();
   int currentPage = 0;
+
+  _changePage(int page) {
+    pageViewController.animateToPage(
+      page,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.easeInOut,
+    );
+    
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       top: false,
       child: Scaffold(
-        body: pages.elementAt(currentPage),
+        body: PageView(
+          controller: pageViewController,
+          onPageChanged: (index) {
+            setState(() {
+              currentPage = index;
+            });
+          },
+          children: pages,
+        ),
         bottomNavigationBar: SizedBox(
           height: 70,
           child: Padding(
@@ -36,6 +55,8 @@ class _HomeScreenState extends State<HomeScreen> {
                 HomeNavigatorItem(
                   onPressed: () => setState(() {
                     currentPage = 0;
+                    pageViewController.jumpToPage(currentPage);
+                    _changePage(currentPage);
                   }),
                   title: TextImages.homeNavigatorTitle1,
                   svg: TextImages.homeNavigatorIcon1,
@@ -44,6 +65,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 HomeNavigatorItem(
                   onPressed: () => setState(() {
                     currentPage = 1;
+                    _changePage(currentPage);
                   }),
                   title: TextImages.homeNavigatorTitle2,
                   svg: TextImages.homeNavigatorIcon2,
@@ -52,6 +74,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 HomeNavigatorItem(
                   onPressed: () => setState(() {
                     currentPage = 2;
+                    _changePage(currentPage);
                   }),
                   title: TextImages.homeNavigatorTitle3,
                   svg: TextImages.homeNavigatorIcon3,
@@ -60,6 +83,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 HomeNavigatorItem(
                   onPressed: () => setState(() {
                     currentPage = 3;
+                    _changePage(currentPage);
                   }),
                   title: TextImages.homeNavigatorTitle4,
                   svg: TextImages.homeNavigatorIcon4,
